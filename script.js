baebfe80ebde2ba4c3d11a41ef7fb3646c97e81e
Koundinya G <121346044+Koundu@users.noreply.github.com>
2024-08-17 23:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+var res = [];
+for(let i=1;i<=100;i++){
+	if(i%5 === 0 && i%3 === 0){
+		res.push("FizzBuzz")
+	}
+	else if(i%3 === 0 && i%5 !== 0){
+		res.push("Fizz");
+	}else if(i%5 === 0 && i%3 !== 0){
+		res.push("Buzz");
+	}else{
+		res.push(i);
+	}
+}
+
+for(let i=0;i<=res.length-1;i++){
+	alert(res[i]+"\n");
+}
